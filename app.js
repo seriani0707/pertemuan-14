75e6cfd71f96655d3fb527fa31155b8adfa25ba8
seriani0707 <seriyani774@gmail.com>
2023-01-05 14:53:03 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// import express dan routing
+const express = require("express");
+const router = require("./routes/api.js");
+
+// Membuat object express
+const app = express();
+
+// Menggunakan middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Menggunakan routing (router)
+app.use(router);
+
+// Mendefinisikan port.
+const PORT = 3000;
+app.listen(PORT, () =>
+console.log('Serever running at: http://localhost:${PORT}')
+);
---
+++ data/students.js
@@ -0,0 +1,7 @@
+// TODO 1: Buat data students
+// code here
+const students = ["Mulia","Muharram","Yahya"];
+
+// TODO 2: export data students
+// code here
+module.exports = students;
